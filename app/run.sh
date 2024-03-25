3b201928b1ee09711f1382f235cc728b231fa0bc
HenriPucci <pucci.rique1234@gmail.com>
2024-03-24 23:51:47 -0300

    Adicionando script de run.sh

--- app/run.sh
+++ app/run.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
+# Start the Python backend
+python3 app.py &
+
+# Store the PID of the Python process
+PYTHON_PID=$!
+
+# Start the Vue.js frontend
+cd cb_viagens_app
+yarn serve
+
+# Once the frontend server is stopped, kill the Python process
+kill $PYTHON_PID
+
 echo "Implemente aqui o script para executar a sua solução"
